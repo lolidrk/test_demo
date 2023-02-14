e906d64059a6dc36bc84ddfab44a9e2818973d0d
lolidrk <kalyanikulkarni2002@gmail.com>
2023-02-14 11:06:40 +0530

    Corrected the error

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "Now it's time to party";
-  cout << "dance dance "
+  cout << "dance dance ";
   return 0;
 }
