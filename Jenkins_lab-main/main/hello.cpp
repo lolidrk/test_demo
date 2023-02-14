02ac19cf79df0df85beb2497d80242ba64e3b87b
lolidrk <kalyanikulkarni2002@gmail.com>
2023-02-14 21:09:55 +0530

    Making changes

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Now it's time to party";
-  cout << "dance dance ";
+  cout << "Idk how many times I have done this now";
   return 0;
 }
