a212a16f350141dd06184a86c277ec797b29260c
lolidrk <kalyanikulkarni2002@gmail.com>
2023-02-14 21:23:44 +0530

    i will actually cry

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -6,8 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Idk how many times I have done this now";
-  cout << "I will actually cry now";
-  
+  cout << "Idk how many times I have done this now\n";
+  cout << "I will actually cry now\n";
+  cout << "I'm not lying\n";
   return 0;
 }
