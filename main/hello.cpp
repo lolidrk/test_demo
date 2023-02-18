4b396772c077b79f0c976411a3c1b4f8e96ce860
lolidrk <kalyanikulkarni2002@gmail.com>
2023-02-18 11:18:30 +0530

    i can't anymore

---
+++ main/hello.cpp
@@ -0,0 +1,13 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Idk how many times I have done this now\n";
+  cout << "I will actually cry now\n";
+  cout << "I'm not lying\n";
+  return 0;
+}
